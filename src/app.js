e33bf32ee2113e2fce32bec2d686dc01c8a55a7c
brodrigues0ll <bernardorg99@hotmail.com>
2023-11-21 16:06:23 -0300

    Back OK

--- src/app.js
+++ src/app.js
@@ -1,33 +1,67 @@
-import express from "express";
-import http from "http";
-import cors from "cors";
-import { Server } from "socket.io";
-import tmi from "tmi.js";
-import errorMiddleware from "./middlewares/errorMiddleware.js";
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const tmi = require("tmi.js");
+const cors = require("cors");
 
 const app = express();
-const server = http.createServer(app);
-
 app.use(express.json());
 app.use(cors());
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const APP_PORT = process.env.PORT || 3001;
 
 app.get("/", (_, res) => res.json({ ok: "app running" }));
 
-app.post("/startConnection", (req, res) => {
-  const { streamerChannelName } = req.body;
-  const client = new tmi.Client({
-    channels: [streamerChannelName],
+server.listen(APP_PORT, () => console.log(`App listening on port ${APP_PORT}`));
+
+let messages = [];
+
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    messages.push(message);
+    console.log("Received: %s", message);
+
+    const parsedMessage = JSON.parse(message);
+
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(parsedMessage));
+      }
+    });
   });
 
-  client.connect();
-
-  client.on("message", (channel, tags, message, self) => {
-    console.log(`${tags["display-name"]}: ${message}`);
-  });
-
-  return res.status(200).json({ status: "connected" });
+  ws.send(JSON.stringify(messages));
 });
 
-app.use(errorMiddleware);
+app.post("/startConnection", (req, res) => {
+  try {
+    const { streamerChannelName } = req.body;
+    const client = new tmi.Client({
+      channels: [streamerChannelName],
+    });
 
-export default app;
+    client.connect();
+
+    client.on("message", (channel, tags, message, self) => {
+      if (self) return;
+
+      const data = {
+        channel,
+        tags,
+        message: `${tags["display-name"]}: ${message}`,
+      };
+
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(data));
+        }
+      });
+    });
+
+    return res.status(200).json({ status: "connected" });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
