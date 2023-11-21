7cc6879162db5df771073465cf0f05ff20625f57
brodrigues0ll <bernardorg99@hotmail.com>
2023-11-21 15:03:26 -0300

    Clean Back

--- src/app.js
+++ src/app.js
@@ -8,26 +8,8 @@ import errorMiddleware from "./middlewares/errorMiddleware.js";
 const app = express();
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000/",
-    methods: ["GET", "POST"],
-    credentials: true,
-    allowedHeaders: ["Content-Type"],
-  },
-});
-
-let clientSocket;
-
 app.use(express.json());
 app.use(cors());
-
-io.on("connection", (socket) => {
-  console.log("a user connected");
-
-  clientSocket = socket;
-  console.log(clientSocket);
-});
 
 app.get("/", (_, res) => res.json({ ok: "app running" }));
 
@@ -41,12 +23,6 @@ app.post("/startConnection", (req, res) => {
 
   client.on("message", (channel, tags, message, self) => {
     console.log(`${tags["display-name"]}: ${message}`);
-    if (channel.split("#")[1] === streamerChannelName) {
-      const newMessage = { username: tags["display-name"], message };
-      if (clientSocket) {
-        clientSocket.emit("newMessage", newMessage); // Emite um evento quando uma nova mensagem é recebida para todos os clientes conectados
-      }
-    }
   });
 
   return res.status(200).json({ status: "connected" });
